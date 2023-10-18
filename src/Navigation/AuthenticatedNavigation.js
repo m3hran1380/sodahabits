06c1f2110d49404c7c3510aec429b77d6f2ea0aa
Mehran Parvareshnia <mparvareshnia80@gmail.com>
2023-10-18 23:46:43 +0100

    added the basics of the functionality for running functions at specific times (local time)

--- src/Navigation/AuthenticatedNavigation.js
+++ src/Navigation/AuthenticatedNavigation.js
@@ -4,6 +4,8 @@ import SocialScreen from '../screens/mainScreens/SocialScreen';
 import VendingMachineScreen from '../screens/mainScreens/VendingMachineScreen';
 import LocationScreen from '../screens/mainScreens/LocationScreen';
 import TabBarButton from '../components/navigationComponents/TabBarButton';
+import React, { useEffect, useState, useCallback } from 'react';
+import { AppState } from 'react-native';
 
 
 const Tab = createBottomTabNavigator();
@@ -19,6 +21,56 @@ const AuthenticatedNavigation = () => {
         position: 'absolute',
         paddingBottom: 10,
     }
+
+    const [appState, setAppState] = useState(AppState.currentState);
+
+    // this senction involves the timers that are set out to perform actions in specific times
+    const executeAtMidnight = () => {
+        console.log('It is now midnight!');
+        // Perform your actions here
+    };    
+    const setMidnightTimer = useCallback(() => {
+        const now = new Date();
+        const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 46, 0);
+        const msUntilMidnight = midnight.getTime() - now.getTime();
+    
+        // Set a new timer
+        return setTimeout(executeAtMidnight, msUntilMidnight);
+    }, []);
+
+    useEffect(() => {
+        let timerId;
+        // Set up a listener for app state changes
+        const subscription = AppState.addEventListener('change', (nextAppState) => {
+          if (
+            appState.match(/inactive|background/) &&
+            nextAppState === 'active'
+          ) {
+            console.log('App has come to the foreground!');
+            timerId = setMidnightTimer(); // set a new timer when app comes to the foreground
+          } else if (
+            appState === 'active' &&
+            nextAppState.match(/inactive|background/)
+          ) {
+            console.log('App is going to the background!');
+            clearTimeout(timerId); // clear the existing timer when app goes to the background
+          }
+    
+          // Update the app state
+          setAppState(nextAppState);
+        });
+    
+        // Set the initial timer
+        timerId = setMidnightTimer();
+    
+        return () => {
+          // Cleanup function
+          subscription.remove();
+          clearTimeout(timerId); // make sure to clear the timer when the component unmounts
+        };
+    }, [appState, setMidnightTimer]);
+
+
 
     return (
         <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false, tabBarStyle: {...barStyle}}}>
